ad1377c16830f2afe0f3aceb556d3c32e398cd68
Roheena <palroheena@gmail.com>
2022-01-27 20:00:03 +0530

    commit

---
+++ notice_index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>Download PDF using PHP from HTML link</title>
+</head>
+
+<body>
+    <center>
+        <h2 style="color:#d7385e;">View the Notices</h2>
+        <p><b>Click below to download PDF</b></p>
+        <a href="" target="">Download PDF Now</a>
+    </center>
+</body>
+</html>
